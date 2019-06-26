287f90245e3bf727632572bd63de920941c45901
lakshmishama <lakshmishama@users.noreply.github.com>
2019-06-26 10:47:05 +0530

    Add files via upload

---
+++ 3.DataTypes.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+    /*integers*/
+    int a = 5, b = 4;
+    int c;
+    c = a + b;
+    printf("integer a= %d, b = %d , Sum = %d \n",a,b,c);
+    /* Float */
+    float p = 5.5;
+    printf("float p = %f \n", p);
+    /*Char */
+    char gender = 'M';
+    printf("Char gender = %c \n", gender);
+    /*unsigned int, int, unsigned long, long, float, double, long double, unsigned char, char*/
+}
